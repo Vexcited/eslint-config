feb8e516ce374947168086107ea7807dec514bfc
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2026-01-14 21:17:34 +0100

    fix: upgrade perfectionist to v5

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -10,24 +10,27 @@ export function perfectionist(): TypedFlatConfigItem[] {
         perfectionist: pluginPerfectionist
       },
       rules: {
-        ...pluginPerfectionist.configs["recommended-natural"].rules,
+        "perfectionist/sort-exports": ["error", { order: "asc", type: "natural" }],
         "perfectionist/sort-imports": ["error", {
           groups: [
-            "type",
-            ["parent-type", "sibling-type", "index-type", "internal-type"],
+            "type-import",
+            ["type-parent", "type-sibling", "type-index", "type-internal"],
 
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling", "index"],
+            "value-builtin",
+            "value-external",
+            "value-internal",
+            ["value-parent", "value-sibling", "value-index"],
             "side-effect",
-            "object",
+            "ts-equals-import",
             "unknown"
           ],
           newlinesBetween: "ignore",
+          newlinesInside: "ignore",
           order: "asc",
           type: "natural"
-        }]
+        }],
+        "perfectionist/sort-named-exports": ["error", { order: "asc", type: "natural" }],
+        "perfectionist/sort-named-imports": ["error", { order: "asc", type: "natural" }]
       }
     }
   ];
